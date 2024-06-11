bd6fdf344d3fd8a5cd43c2d50c69fdaf7b87bba4
YehiaEhab16 <yehiaorfy3@gmail.com>
2024-06-11 16:47:27 +0300

    Testing different dataset

--- helper_funcs.py
+++ helper_funcs.py
@@ -8,49 +8,47 @@ import matplotlib.pyplot as plt
 
 # Plot Transformed Images
 def plot_transformed_images(image_paths, transform, n=3):
+    # Select Random Images
     random_image_paths = random.sample(image_paths, k=n)
     for image_path in random_image_paths:
         with Image.open(image_path) as f:
             fig, ax = plt.subplots(1, 2,figsize=(5,3.5))
+            # Plot original image
             ax[0].imshow(f) 
             ax[0].set_title(f"Original \nSize: {f.size}")
             ax[0].axis("off")
 
+            # Plot images after transform
             transformed_image = transform(f).permute(1, 2, 0) 
             ax[1].imshow(transformed_image) 
             ax[1].set_title(f"Transformed \nSize: {transformed_image.shape}")
             ax[1].axis("off")
 
+            # Plot title
             fig.suptitle(f"Class: {image_path.parent.stem}", fontsize=12)
 
-def train_step(model: torch.nn.Module, dataloader: torch.utils.data.DataLoader, loss_fn: torch.nn.Module, optimizer: torch.optim.Optimizer, device: str):
+# Training Step
+def train_step(model: torch.nn.Module, dataloader: torch.utils.data.DataLoader, loss_fn: torch.nn.Module, optimizer: torch.optim.Optimizer, device: torch.device):
     # Put model in train mode
     model.train()
-    
     # Setup train loss and train accuracy values
     train_loss, train_acc = 0, 0
-    
+
     # Loop through data loader data batches
     for batch, (X, y) in enumerate(dataloader):
         # Send data to target device
         X, y = X.to(device), y.to(device)
-
         # 1. Forward pass
         y_pred = model(X)
-
         # 2. Calculate  and accumulate loss
         loss = loss_fn(y_pred, y)
         train_loss += loss.item() 
-
         # 3. Optimizer zero grad
         optimizer.zero_grad()
-
         # 4. Loss backward
         loss.backward()
-
         # 5. Optimizer step
         optimizer.step()
-
         # Calculate and accumulate accuracy metric across all batches
         y_pred_class = torch.argmax(torch.softmax(y_pred, dim=1), dim=1)
         train_acc += (y_pred_class == y).sum().item()/len(y_pred)
@@ -60,10 +58,10 @@ def train_step(model: torch.nn.Module, dataloader: torch.utils.data.DataLoader, 
     train_acc = train_acc / len(dataloader)
     return train_loss, train_acc
 
-def test_step(model: torch.nn.Module, dataloader: torch.utils.data.DataLoader, loss_fn: torch.nn.Module, device: str):
+# Test Step
+def test_step(model: torch.nn.Module, dataloader: torch.utils.data.DataLoader, loss_fn: torch.nn.Module, device: torch.device):
     # Put model in eval mode
     model.eval() 
-    
     # Setup test loss and test accuracy values
     test_loss, test_acc = 0, 0
     
@@ -73,14 +71,11 @@ def test_step(model: torch.nn.Module, dataloader: torch.utils.data.DataLoader, l
         for batch, (X, y) in enumerate(dataloader):
             # Send data to target device
             X, y = X.to(device), y.to(device)
-    
             # 1. Forward pass
             test_pred_logits = model(X)
-
             # 2. Calculate and accumulate loss
             loss = loss_fn(test_pred_logits, y)
             test_loss += loss.item()
-            
             # Calculate and accumulate accuracy
             test_pred_labels = test_pred_logits.argmax(dim=1)
             test_acc += ((test_pred_labels == y).sum().item()/len(test_pred_labels))
@@ -90,44 +85,40 @@ def test_step(model: torch.nn.Module, dataloader: torch.utils.data.DataLoader, l
     test_acc = test_acc / len(dataloader)
     return test_loss, test_acc
 
+# Training Loop
 def train_model(model: torch.nn.Module, train_dataloader: torch.utils.data.DataLoader, test_dataloader: torch.utils.data.DataLoader, 
-          optimizer: torch.optim.Optimizer, loss_fn: torch.nn.Module, epochs: int, device:str):
+          optimizer: torch.optim.Optimizer, loss_fn: torch.nn.Module, epochs: int, device:torch.device):
     
-    # 2. Create empty results dictionary
+    # 1. Create empty results dictionary
     results = {"train_loss": [], "train_acc": [], "test_loss": [], "test_acc": []}
     
-    # 3. Loop through training and testing steps for a number of epochs
+    # 2. Loop through training and testing steps for a number of epochs
     for epoch in tqdm(range(epochs)):
         train_loss, train_acc = train_step(model=model, dataloader=train_dataloader, loss_fn=loss_fn, optimizer=optimizer,device=device)
         test_loss, test_acc = test_step(model=model, dataloader=test_dataloader, loss_fn=loss_fn,device=device)
-        
-        # 4. Print out what's happening
-        print(f"Epoch: {epoch+1} | "f"train_loss: {train_loss:.4f} | "f"train_acc: {train_acc:.4f} | "f"test_loss: {test_loss:.4f} | "f"test_acc: {test_acc:.4f}"
-        )
-
-        # 5. Update results dictionary
+        # 3. Print results
+        print(f"Epoch: {epoch+1} | "f"train_loss: {train_loss:.4f} | "f"train_acc: {train_acc:.4f} | "f"test_loss: {test_loss:.4f} | "f"test_acc: {test_acc:.4f}")
+        # 4. Update results dictionary
         results["train_loss"].append(train_loss)
         results["train_acc"].append(train_acc)
         results["test_loss"].append(test_loss)
         results["test_acc"].append(test_acc)
 
-    # 6. Return the filled results at the end of the epochs
+    # 5. Return the filled results at the end of the epochs
     return results
 
+# Plot loss and accuracy
 def plot_loss_curves(results: dict[str, list[float]]):
-
+    # Get loss values
     loss = results['train_loss']
     test_loss = results['test_loss']
-
     # Get the accuracy values of the results dictionary (training and test)
     accuracy = results['train_acc']
     test_accuracy = results['test_acc']
-
     # Figure out how many epochs there were
     epochs = range(len(results['train_loss']))
-
     # Setup a plot 
-    plt.figure(figsize=(15, 7))
+    plt.figure(figsize=(10, 5))
 
     # Plot loss
     plt.subplot(1, 2, 1)
@@ -145,36 +136,30 @@ def plot_loss_curves(results: dict[str, list[float]]):
     plt.xlabel('Epochs')
     plt.legend()
 
-def pred_and_plot_image(model: torch.nn.Module, image_path: str, class_names: list[str], device: torch.device, transform=None):
-    """Makes a prediction on a target image and plots the image with its prediction."""
-    
+# Predict custom image
+def pred_and_plot_image(model: torch.nn.Module, image_path: str, class_names: list[str], device: torch.device, transform=None):    
     # 1. Load in image and convert the tensor values to float32
     target_image = torchvision.io.read_image(str(image_path)).type(torch.float32)
-    
     # 2. Divide the image pixel values by 255 to get them between [0, 1]
     target_image = target_image / 255. 
-    
-    print('Yehia', target_image)
-
     # 3. Transform if necessary
     if transform:
         target_image = transform(target_image)
     
     # 4. Make sure the model is on the target device
     model.to(device)
-    
     # 5. Turn on model evaluation mode and inference mode
     model.eval()
+
+    # Turn on inference context manager
     with torch.inference_mode():
         # Add an extra dimension to the image
         target_image = target_image.unsqueeze(dim=0)
-    
         # Make a prediction on image with an extra dimension and send it to the target device
         target_image_pred = model(target_image.to(device))
         
     # 6. Convert logits -> prediction probabilities (using torch.softmax() for multi-class classification)
     target_image_pred_probs = torch.softmax(target_image_pred, dim=1)
-
     # 7. Convert prediction probabilities -> prediction labels
     target_image_pred_label = torch.argmax(target_image_pred_probs, dim=1)
     
@@ -186,3 +171,4 @@ def pred_and_plot_image(model: torch.nn.Module, image_path: str, class_names: li
         title = f"Pred: {target_image_pred_label} | Prob: {target_image_pred_probs.max().cpu():.3f}"
     plt.title(title)
     plt.axis(False)
+    
